40691cbd84c0b57cec893f18bd1d584f06d27b94
Madhav Shroff <madhavshroff99@gmail.com>
2020-08-29 18:58:28 +0530

    Created server with auth using JWT, request validation, unit and integreation tests, continous integration, documentation and docker support

---
+++ server/src/docs/swaggerDef.js
@@ -0,0 +1,17 @@
+const { version } = require('../../package.json');
+const config = require('../config/config');
+
+const swaggerDef = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Player6 Backend API documentation',
+    version,
+  },
+  servers: [
+    {
+      url: `http://localhost:${config.port}/v1`,
+    },
+  ],
+};
+
+module.exports = swaggerDef;
