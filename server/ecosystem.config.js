756639303e0ed3be0a18b7db53d7d53309da69ac
Madhav Shroff <madhavshroff99@gmail.com>
2020-09-06 05:39:10 +0530

    added call through pm2-runtime

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "apps": [
+  apps: [
     {
-      "name": "app",
-      "script": "src/index.js",
-      "instances": 1,
-      "autorestart": true,
-      "watch": false,
-      "time": true,
-      "env": {
-        "NODE_ENV": "production"
-      }
-    }
-  ]
-}
+      name: 'app',
+      script: 'src/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      time: true,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
